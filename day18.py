9fa5f867b8b04f2416609a459b5f4a149e710cc9
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-02 12:10:43 +0700

    exercises

---
+++ day18.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("blue", "black")
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
